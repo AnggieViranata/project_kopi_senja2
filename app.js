5eacd6cc3b34c5e259ab0bd776d1505af0a2dcf1
Anggie Viranata <anggieviranata22@gmail.com>
2025-06-25 20:20:09 +0700

    menambahkan produk ke keranjang dan harga, jumlah dan total

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-  document.addEventListener('alpine:init', () => {
-        Alpine.data('products', () => ({
-
-            items: [
-
-                {id: 1, name: 'coffe_torrado_1', img: 'coffe_torrado_1.jpeg', price: 20000},
-
-                {id: 2, name: 'classic_coffe_2', img: 'classic_coffe_2.jpeg', price: 25000},
-
-                {id: 3, name: 'excellence_3', img: 'excellence_3.jpeg', price: 30000},
-
-                {id: 4, name: 'toraja_coffees_4', img: 'toraja_coffees_4.jpeg', price: 35000},
-
-                {id: 5, name: 'coffee_beans_5', img: 'coffee_beans_5.jpeg', price: 40000},
-            ],
-        }))
+document.addEventListener('alpine:init', () => {
+    Alpine.data('products', () => ({
+        items: [
+            {id: 1, name: 'coffe_torrado_1', img: 'coffe_torrado_1.jpeg', price: 20000},
+            {id: 2, name: 'classic_coffe_2', img: 'classic_coffe_2.jpeg', price: 25000},
+            {id: 3, name: 'excellence_3', img: 'excellence_3.jpeg', price: 30000},
+            {id: 4, name: 'toraja_coffees_4', img: 'toraja_coffees_4.jpeg', price: 35000},
+            {id: 5, name: 'coffee_beans_5', img: 'coffee_beans_5.jpeg', price: 40000},
+        ]
+    }));
 
 
 
-
-Alpine.store('cart', {
-   items: [],
-   total: 0,
-   quantity: 0,
- add(newItem) {
-
-    this.items.push(newItem);
-    this.quantity++;
-    this.total += newItem.price;
-    console.log(this.items);
-    },
+ Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+            const cartItem = this.items.find(item => item.id === newItem.id);
+            if (!cartItem) {
+                this.items.push({...newItem, quantity: 1, total: newItem.price});
+                this.quantity++;
+                this.total += newItem.price;
+            } else {
+                this.items = this.items.map(item => {
+                    if (item.id !== newItem.id) return item;
+                    item.quantity++;
+                    item.total = item.price * item.quantity;
+                    this.quantity++;
+                    this.total += item.price;
+                    return item;
+                });
+            }
+        }
+    });
 });
 
 
-    });
 
 
 
